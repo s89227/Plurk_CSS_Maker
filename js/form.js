a5803a30ef285224069c403b8a14567329d416af
Helen Liao <helenhelens89227@gmail.com>
2016-02-21 02:41:52 +0800

    update form
    add filter_tab
    add updater

--- js/form.js
+++ js/form.js
@@ -1,3 +1,11 @@
 function changeDisplay(target, effect){
 	document.getElementById(target).style.display=effect;
 }
+
+function changeDisplayClass(target, effect){
+	var x = document.getElementsByClassName(target);
+	var i;
+	for (i = 0; i < x.length; i++) {
+    	x[i].style.display = effect;
+	}
+}
